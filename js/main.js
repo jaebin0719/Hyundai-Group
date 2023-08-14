c921411f9a37e5bc18d8603ce3017ceac03884fe
ParkJaeBin <flxksldk@gmail.com>
2023-08-14 11:59:24 +0900

    현대그룹 시작

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  // 메뉴기능
+  const nav = document.querySelector(".nav");
+  const btMenu = document.querySelector(".bt-menu");
+  const navClose = document.querySelector(".nav-close");
+  btMenu.addEventListener("click", function () {
+    // 클래스를 nav에 추가하고 싶다.
+    nav.classList.add("nav-active");
+  });
+  navClose.addEventListener("click", function () {
+    nav.classList.remove("nav-active");
+  });
+  nav.addEventListener("mouseleave",function(){
+    nav.classList.remove("nav-active")
+  })
+};
